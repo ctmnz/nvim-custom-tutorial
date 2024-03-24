aed1c0c2405e6e55da2b0c5192328070b00ab4da
Daniel Stoinov <danie.stoinov@gmail.com>
2024-03-24 16:28:03 +0200

    created create_vsplit() function that automates the debug binding

--- plugin/hello.lua
+++ plugin/hello.lua
@@ -1,4 +1,4 @@
-print("Hello there!")
+print("stnv debug module loaded. use :lua create_vsplit()")
 
 attach_to_buffer = function(output_bufnr, pattern, command)
 vim.api.nvim_create_autocmd("BufWritePost", {
@@ -21,14 +21,38 @@ vim.api.nvim_create_autocmd("BufWritePost", {
 })
 end
 
-print_buffer = function()
-  print(vim.api.nvim_get_current_buf())
-end
-
 stnv_help = function()
   print('example:lua attach_to_buffer(3, "*.py", { "python3", "test.py" })')
 end
 
+debug_help = function()
+  print('example:lua create_vsplit()')
+end
+
+
+
+create_vsplit = function()
+    -- Save the current buffer number
+    local current_bufnr = vim.api.nvim_get_current_buf()
+    local fname =  vim.fn.expand('%:t')
+    local extension = vim.fn.expand('%:e')
+
+    local lang_dict = {
+      ["py"] = "python3 ",
+      ["go"] = "go run",
+      ["lua"] = "lua ",
+    }
+    -- Create a vsplit
+    vim.cmd('vsplit new_window')
+    local new_bufnr = vim.api.nvim_get_current_buf()
+    
+    -- Go back to the original buffer
+    -- vim.api.nvim_set_current_buf(current_bufnr)
+    -- Return the buffer number of the new split
+    print(new_bufnr, "*."..extension ,lang_dict[extension]..fname)
+    attach_to_buffer(new_bufnr, "*."..extension, lang_dict[extension]..fname)
+    return new_bufnr
+end
 
 -- attach_to_buffer(3, "*.py", { "python3", "test.py" })
 -- attach_to_buffer(3, "*.lua", { "lua", "filename.lua" })
