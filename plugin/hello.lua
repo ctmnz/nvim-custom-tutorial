2be4320f720d5aa93d94426ace42841c480f6756
Daniel Stoinov <danie.stoinov@gmail.com>
2024-03-24 14:46:33 +0200

    added attach_to_buffer function

--- plugin/hello.lua
+++ plugin/hello.lua
@@ -1,3 +1,24 @@
 print("Hello there!")
 
+local attach_to_buffer = function(output_bufnr, pattern, command)
+vim.api.nvim_create_autocmd("BufWritePost", {
+  group = vim.api.nvim_create_augroup("Luapocs", { clear = true }),
+  pattern = pattern,
+  callback = function()
+    local append_data = function(_, data)
+      if data then
+        vim.api.nvim_buf_set_lines(output_bufnr, -1, -1, false, data)
+      end
+    end
 
+    vim.api.nvim_buf_set_lines(output_bufnr, 0, -1, flase, { "===== EXECUTION WINDOW =====" })
+    vim.fn.jobstart(command, {
+      stdout_buffered = true,
+      on_stdout = append_data,
+      on_stderr = append_data,
+    })
+  end,
+})
+end
+
+-- attach_to_buffer(3, "*.py", { "python3", "test.py" })
